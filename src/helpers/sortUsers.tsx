71de63bf755c86d6e60c3c624a5d38916aa95bef
dimaraz <dimaraz@ukr.net>
2022-09-21 10:57:26 +0300

    fix 1

--- src/helpers/sortUsers.tsx
+++ src/helpers/sortUsers.tsx
@@ -3,6 +3,7 @@ import { data, generateUserCardList } from '../App';
 export const sortUsers = (setUsers: Function) => {
   data.users.sort((a, b) => (a.name < b.name ? -1 : 1));
   data.startPosition = 0;
-  data.usersFiltered = data.users.filter(elem => elem.name.includes(data.filter));
+  const findStr = data.filter.toUpperCase();
+  data.usersFiltered = data.users.filter(elem => elem.name.toUpperCase().includes(findStr));
   setUsers(generateUserCardList());
 };
--- src/theme/main.tsx
+++ src/theme/main.tsx
@@ -4,27 +4,27 @@ export const bodyBackgroundColor = '#12122D';
 export const cardBackgroundColor = '#333348';
 export const buttonBackgroundColor = '#52519D';
 
-// const for develop
-export const userCardWidth = 210;
-export const userCardHeight = 180;
-export const marginForButtonsX = 112;
-export const marginForButtonsY = 33;
-export const fontSize = {
-  SMALL: 12,
-  NORMAL: 18,
-  BIG: 24,
-};
-
-// // const for product
-// export const userCardWidth = 420;
-// export const userCardHeight = 370;
-// export const marginForButtonsX = 225;
-// export const marginForButtonsY = 75;
+// // const for develop
+// export const userCardWidth = 210;
+// export const userCardHeight = 180;
+// export const marginForButtonsX = 112;
+// export const marginForButtonsY = 33;
 // export const fontSize = {
-//   SMALL: 18,
-//   NORMAL: 24,
-//   BIG: 30,
+//   SMALL: 12,
+//   NORMAL: 18,
+//   BIG: 24,
 // };
+
+// const for product
+export const userCardWidth = 420;
+export const userCardHeight = 370;
+export const marginForButtonsX = 225;
+export const marginForButtonsY = 75;
+export const fontSize = {
+  SMALL: 18,
+  NORMAL: 24,
+  BIG: 30,
+};
 
 export const buttonWidth = {
   SMALL: 200,
